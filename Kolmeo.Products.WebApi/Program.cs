7163d86b6f661b7b5c77ab3a30a6b22725c57bbc
Andrew Cheong <andrew.cheong1986@gmail.com>
2022-07-03 18:55:30 +1000

    Basic features done.

--- Kolmeo.Products.WebApi/Program.cs
+++ Kolmeo.Products.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Kolmeo.Products.Bootstrapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddKolmeoProductServices();
 
 var app = builder.Build();
+
+// Seed data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    DataSeeder.SeedData(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
